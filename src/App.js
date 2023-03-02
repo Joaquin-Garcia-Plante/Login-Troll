2824d669eac3224ae87fa6b617e8ed0cef48f318
joaquin-garcia-plante <joaquingarciaplante@gmail.com>
2023-03-01 21:06:37 -0300

    mostrar y ocultar contraseña

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
 import styles from "./App.module.css";
-import { AiFillEyeInvisible } from "react-icons/ai";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { useState } from "react";
 
 function App() {
   const [checked, setChecked] = useState(false);
-  console.log(checked);
+  const [viewPassword, setViewPassword] = useState(false);
+  const [viewPasswordRep, setViewPasswordRep] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordRep, setPasswordRep] = useState("");
+
+  const handleSetPW = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSetPWRep = (e) => {
+    setPasswordRep(e.target.value);
+  };
+
   return (
     <div>
       <form className={styles.formContainer} autocomplete="off">
-        <input
-          type="text"
-          placeholder="Ingrese su email"
-          className={styles.inputEmail}
-        />
+        <div>
+          <input
+            type="text"
+            placeholder="Ingrese su email"
+            className={styles.inputEmail}
+          />
+        </div>
         <br />
         <div className={styles.inputPwContainer}>
           <input
-            type="password"
+            value={password}
+            type={viewPassword ? "text" : "password"}
             placeholder="Ingrese su contraseña"
             className={styles.inputPassword}
+            onChange={(e) => handleSetPW(e)}
           />
-          <div className={styles.btnEyeIcon}>
-            <AiFillEyeInvisible className={styles.eyeIcon}></AiFillEyeInvisible>
+          <div
+            className={styles.btnEyeIcon}
+            onClick={() => setViewPassword(!viewPassword)}
+          >
+            {viewPassword ? (
+              <AiFillEye className={styles.eyeIcon}></AiFillEye>
+            ) : (
+              <AiFillEyeInvisible
+                className={styles.eyeIcon}
+              ></AiFillEyeInvisible>
+            )}
+          </div>
+        </div>
+        <br />
+        <div className={styles.inputPwContainer}>
+          <input
+            value={passwordRep}
+            type={viewPasswordRep ? "text" : "password"}
+            placeholder="Repita su contraseña"
+            className={styles.inputPassword}
+            onChange={(e) => handleSetPWRep(e)}
+          />
+          <div
+            className={styles.btnEyeIcon}
+            onClick={() => setViewPasswordRep(!viewPasswordRep)}
+          >
+            {viewPasswordRep ? (
+              <AiFillEye className={styles.eyeIcon}></AiFillEye>
+            ) : (
+              <AiFillEyeInvisible
+                className={styles.eyeIcon}
+              ></AiFillEyeInvisible>
+            )}
           </div>
         </div>
         <br />
@@ -31,14 +78,12 @@ function App() {
         </div>
         <br />
         {checked ? (
-          <div className={styles.btnSubmit}>
-            <input type="submit" />
+          <div className={styles.btnSubmitNoTrollContainer}>
+            <input type="submit" className={styles.btnSubmit} />
           </div>
         ) : (
           <div className={styles.btnSubmitContainer}>
-            <div className={styles.btnSubmitTroll}>
-              <input type="submit" />
-            </div>
+            <input type="submit" className={styles.btnSubmitTroll} />
           </div>
         )}
       </form>
